abe2a7fe717cfd5a361e35112b11ef50d1d9b038
aLEGEND21 <arnavmurthi@gmail.com>
2024-06-10 16:15:43 -0400

    chore: Create Internship model

---
+++ server/src/models/internship.ts
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+interface IInternship {
+    title: string;
+    description: string;
+    companyName: string;
+    tags: string[];
+    location: string;
+    startDate: Date;
+    endDate: Date;
+    hoursPerWeek: number;
+    hourlyRate: number;
+    approved: boolean;
+    creator: mongoose.Types.ObjectId;
+}
+
+const internshipSchema = new mongoose.Schema<IInternship>({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    companyName: { type: String, required: true },
+    tags: { type: [String], required: true },
+    location: { type: String, required: true },
+    startDate: { type: Date, required: false },
+    endDate: { type: Date, required: false },
+    hoursPerWeek: { type: Number, required: true },
+    hourlyRate: { type: Number, required: true },
+    approved: { type: Boolean, required: true },
+    creator: { type: mongoose.Schema.Types.ObjectId, required: true },
+}, { timestamps: true });
+
+const Internship = mongoose.model<IInternship>('Internship', internshipSchema);
+
+function createInternship(attr: IInternship) {
+    return new Internship(attr);
+}
+
+export { Internship, IInternship, createInternship };
