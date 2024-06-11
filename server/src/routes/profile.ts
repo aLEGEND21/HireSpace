1b188ca7be5f70b422ae0c12a822f5e421ebe26d
aLEGEND21 <arnavmurthi@gmail.com>
2024-06-11 14:25:25 -0400

    feat: Add route to view other user's profiles

--- server/src/routes/profile.ts
+++ server/src/routes/profile.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { Types } from 'mongoose';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -9,5 +11,29 @@ router.get('/profile/@me', async (req, res) => {
         res.status(401).send('Not authenticated');
     }
 });
+
+router.get('/profile/:id', async (req, res) => {
+    const id = req.params.id;
+
+    // Validate the ID so it doesn't crash the server if it's invalid
+    if (!Types.ObjectId.isValid(id)) {
+        return res.status(400).send('Invalid ID');
+    }
+    
+    // Find the user
+    const user = await User.findOne({ _id: req.params.id });
+    if (!user) {
+        return res.status(404).send('User not found');
+    }
+
+    // Remove sensitive information
+    const userObj = user.toObject() as any; // Convert to any so that we can delete properties
+    delete userObj.email;
+    delete userObj.password;
+    delete userObj.salt;
+
+    res.status(200).send(userObj);
+});
+
 
 export { router as profileRouter };
