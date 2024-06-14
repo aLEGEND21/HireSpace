4dd098b350cae47ee9d4ce886730974460e0e3ed
aLEGEND21 <arnavmurthi@gmail.com>
2024-06-14 15:58:16 -0400

    chore: Revamp the login page

--- client/src/routes/login.tsx
+++ client/src/routes/login.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 function Login() {
   const navigate = useNavigate();
@@ -32,34 +33,47 @@ function Login() {
   }
 
   return (
-    <div className="container mx-auto mt-5">
-      <h1 className="text-3xl font-bold">Login</h1>
-      <form className="mt-5">
-        <span className="">Username</span>
-        <input
-          type="text"
-          className="block w-full border border-gray-300 rounded-md mt-1"
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-        <span className="mt-5">Password</span>
-        <input
-          type="password"
-          className="block w-full border border-gray-300 rounded-md mt-1"
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <input
-          type="submit"
-          className="block w-full bg-blue-500 text-white rounded-md mt-5 p-2"
-          value="Log In"
-          onClick={(e) => {
-            handleSubmit(e);
-          }}
-        />
-      </form>
+    <div>
+      <Navbar />
+      <div className="container mx-auto max-w-96">
+        <h1 className="text-5xl font-bold text-center mt-20">Login</h1>
+        <form className="mt-5">
+          <div className="mt-5">
+            <span className="font-semibold">Username</span>
+            <input
+              type="text"
+              className="block w-full border border-gray-300 rounded-md mt-1 py-2 px-3"
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+          </div>
+          <div className="mt-3 mb-5">
+            <span className="font-semibold">Password</span>
+            <input
+              type="password"
+              className="block w-full border border-gray-300 rounded-md mt-1 py-2 px-3"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <input
+            type="submit"
+            className="block w-full bg-black text-white rounded-md p-3"
+            value="Log In"
+            onClick={(e) => {
+              handleSubmit(e);
+            }}
+          />
+        </form>
+        <p className="text-center mt-5">
+          Don't have an account?{" "}
+          <Link to="/register" className="text-blue-500">
+            Register
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
