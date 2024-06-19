dd87b663f12e914742e892f5b5fa8a32296a38ef
aLEGEND21 <arnavmurthi@gmail.com>
2024-06-19 11:08:33 -0400

    feat: Add links to pages in navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { SessionContext } from "../contexts";
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 interface NavbarProps {
   searchBox?: JSX.Element;
@@ -8,8 +9,7 @@ interface NavbarProps {
 
 function Navbar({ searchBox }: NavbarProps) {
   const session = useContext(SessionContext);
-
-  console.log(session);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
   return (
     <nav className="bg-white border-gray-200 border-b">
@@ -29,11 +29,47 @@ function Navbar({ searchBox }: NavbarProps) {
             </li>
             <li>
               {session.loggedIn ? (
-                <Link to="/logout">
-                  <button className="bg-black text-white rounded-lg px-5 py-2">
-                    Logout
+                <>
+                  <button
+                    className="bg-black text-white rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+                    onClick={() => setDropdownOpen(!dropdownOpen)}
+                  >
+                    Actions
+                    <MdKeyboardArrowDown className="text-xl ms-2 mt-0.5" />
                   </button>
-                </Link>
+
+                  {dropdownOpen && (
+                    <div className="absolute divide-y text-sm bg-white divide-gray-200 rounded-lg border border-gray-200 w-44 mt-2">
+                      <div className="px-4 py-2.5 truncate text-gray-600 font-semibold">
+                        @{session.username}
+                      </div>
+                      <ul className="text-sm text-gray-600">
+                        <li>
+                          <Link
+                            to="/internship/submit"
+                            className="block px-4 py-2.5 hover:bg-slate-100"
+                          >
+                            Submit Internship
+                          </Link>
+                          {session.roles.includes("moderator") && (
+                            <Link
+                              to="/internships/pending"
+                              className="block px-4 py-2.5 hover:bg-slate-100"
+                            >
+                              Pending Approval
+                            </Link>
+                          )}
+                        </li>
+                      </ul>
+                      <Link
+                        to="/logout"
+                        className="block px-4 py-2.5 text-sm text-gray-600 hover:bg-slate-100"
+                      >
+                        Log out
+                      </Link>
+                    </div>
+                  )}
+                </>
               ) : (
                 <Link to="/login">
                   <button className="bg-black text-white rounded-lg px-5 py-2">
