f29d011820ef0653232b89db86d36f26d9124cab
aLEGEND21 <arnavmurthi@gmail.com>
2024-06-20 13:31:09 -0400

    refactor: Revamp internship view page UI

--- client/src/routes/view.tsx
+++ client/src/routes/view.tsx
@@ -1,10 +1,27 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 function View() {
   const { id } = useParams<{ id: string }>();
   const [internship, setInternship] = useState<any>(null);
   const [creator, setCreator] = useState<any>(null);
+
+  // Format all dates in the internship data
+  const startDate = new Date(internship?.startDate);
+  const endDate = new Date(internship?.endDate);
+  const submittedDate = new Date(internship?.createdAt);
+  const fmtOptions: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
+  const startDateString = startDate.toLocaleDateString(undefined, fmtOptions);
+  const endDateString = endDate.toLocaleDateString(undefined, fmtOptions);
+  const submittedDateString = submittedDate.toLocaleDateString(
+    undefined,
+    fmtOptions
+  );
 
   useEffect(() => {
     fetch(`http://localhost:3000/internship/${id}`)
@@ -37,27 +54,50 @@ function View() {
   }, [internship?.creator]);
 
   return (
-    <div className="container mx-auto">
-      <h1 className="text-2xl font-bold">View Internship</h1>
-      <h3 className="text-lg mt-5">{internship?.title}</h3>
-      <h4 className="text-md mt-5">Tags: {internship?.tags.toString()}</h4>
-      <p className="mt-5">
-        {internship?.startDate} - {internship?.endDate}
-      </p>
-      <p className="mt-5">{internship?.description}</p>
-      <p className="mt-5">Location: {internship?.location}</p>
-      <p className="mt-5">Company: {internship?.companyName}</p>
-      <p className="mt-5">
-        {internship?.hoursPerWeek} hours per week @ ${internship?.hourlyRate}
-        /hour
-      </p>
-      <p className="mt-5">
-        Application URL:{" "}
-        <a href={internship?.applicationUrl} className="text-blue-500">
-          {internship?.applicationUrl}
+    <div>
+      <Navbar />
+      <div className="container mx-auto my-10">
+        <h1 className="text-3xl text-center font-semibold">
+          {internship?.title}
+        </h1>
+        <p className="text-center text-lg my-1">
+          Posted by <u className="font-semibold">{creator?.username}</u> on{" "}
+          <u className="font-semibold">{submittedDateString}</u>
+        </p>
+        <p className="text-center text-lg text-gray-600 my-1">
+          {internship?.companyName}
+        </p>
+        <p className="text-center text-lg text-gray-600 my-1">
+          {internship?.location} • {startDateString} - {endDateString}
+        </p>
+        <p className="text-center text-lg text-gray-600 my-1">
+          ${internship?.hourlyRate} / hour • {internship?.hoursPerWeek} hours /
+          week
+        </p>
+        <h3 className="text-2xl font-semibold text-center mt-10">
+          Internship Description
+        </h3>
+        <p className="text-justify text-lg mt-2">
+          {/* Safely render newlines in the description */}
+          {internship?.description
+            .split("\n")
+            .map((line: string, i: number) => (
+              <Fragment key={i}>
+                {line}
+                <br />
+              </Fragment>
+            ))}
+        </p>
+        <a
+          href={internship?.applicationUrl}
+          target="_blank"
+          className="flex justify-center"
+        >
+          <button className="bg-black text-white rounded-lg py-3 font-semibold mt-5 w-48">
+            Apply
+          </button>
         </a>
-      </p>
-      <p className="mt-5">Posted by: {creator?.username}</p>
+      </div>
     </div>
   );
 }
