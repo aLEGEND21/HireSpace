9f99647b86b5a38d4982a0f312c1e86ae0517cc1
aLEGEND21 <arnavmurthi@gmail.com>
2024-11-04 15:32:15 -0500

    fix: Attempt to correctly load env vars

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // Determine the path to load the environment variables from
-const envPath = process.env.NODE_ENV === 'production' ? './' : '../'
+console.log('NODE_ENV:', process.env.NODE_ENV)
+const envPath = process.env.NODE_ENV === 'production' ? '../../' : '../'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //envDir: path.resolve(__dirname, envPath),
+  envDir: path.resolve(__dirname, envPath),
 })
