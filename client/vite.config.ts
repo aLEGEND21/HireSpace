c0feabb0ce051fc9eedd08f39894323fce097a8f
aLEGEND21 <arnavmurthi@gmail.com>
2024-11-04 15:25:31 -0500

    build: Dynamically set the .env filepath based on the NODE_ENV

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+// Determine the path to load the environment variables from
+const envPath = process.env.NODE_ENV === 'production' ? './' : '../'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envDir: path.resolve(__dirname, '../'),
+  envDir: path.resolve(__dirname, envPath),
 })
