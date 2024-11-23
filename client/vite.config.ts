754821d401e7b5a5e7da35fa8dd7c86822cf723b
aLEGEND21 <arnavmurthi@gmail.com>
2024-11-22 21:57:21 -0500

    fix: Attempt fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,8 @@ import path from 'path'
 // Determine the path to load the environment variables from
 console.log('NODE_ENV:', process.env.NODE_ENV)
 const envPath = process.env.NODE_ENV === 'production' ? '../../' : '../'
+console.log('envPath:', envPath)
+console.log(process.env)
 
 // https://vitejs.dev/config/
 export default defineConfig({
