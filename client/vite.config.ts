89e235be4e7d7d9dc79ba58b7da7945b04e72217
aLEGEND21 <arnavmurthi@gmail.com>
2024-11-22 22:27:46 -0500

    fix: Attempt fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import dotenv from 'dotenv'
+
 
 // Determine the path to load the environment variables from
+dotenv.config({ path: path.resolve(__dirname, '../.env') })
 console.log('NODE_ENV:', process.env.NODE_ENV)
 const envPath = "../"
 
